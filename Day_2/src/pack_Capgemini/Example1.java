f85257d2c3cf24d742ad15cdf47f463118929794
admin <admin@DESKTOP-3SKLC6M>
2022-05-10 10:34:16 +0530

    first commit

---
+++ Day_2/src/pack_Capgemini/Example1.java
@@ -0,0 +1,19 @@
+package pack_Capgemini;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		int a=15;
+		float b=10.14f;
+		double c=15.1;
+		char d='b';
+		String str="BHoomika Shetty";
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(str);
+
+	}
+
+}
---
+++ Day_2/src/pack_Capgemini/Example2.java
@@ -0,0 +1,20 @@
+package pack_Capgemini;
+
+import java.util.Scanner;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		System.out.println(a);
+		float b=s.nextFloat();
+		System.out.println(b);
+		double c=s.nextDouble();
+		System.out.println(c);
+		
+		s.close();
+
+	}
+
+}
