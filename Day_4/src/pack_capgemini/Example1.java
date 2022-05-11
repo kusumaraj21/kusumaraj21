9d124b7e59b06104fb57c0b9ad8d14965b42f31b
admin <admin@DESKTOP-3SKLC6M>
2022-05-11 16:05:43 +0530

    first commit

---
+++ Day_4/src/pack_capgemini/Example1.java
@@ -0,0 +1,11 @@
+package pack_capgemini;
+
+public class Example1 {
+	public void display(){
+		System.out.println("welcome");
+	
+
+	}
+
+
+}
---
+++ Day_4/src/pack_capgemini/Example2.java
@@ -0,0 +1,11 @@
+package pack_capgemini;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		Example1 obj=new Example1();
+		obj.display();
+
+	}
+
+}
